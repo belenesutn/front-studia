5ff055de7d030fd2a35aa01aad0162c30f0b1c06
Studia <mpartenzi@frd.utn.edu.ar>
2023-05-24 19:19:39 -0300

    fix cookie

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -2,8 +2,9 @@ import axios from "axios";
 
 const newRequest = axios.create({
   baseURL: "https://studia-utn.uc.r.appspot.com/api/",
-  withCredentials: true,
-  headers: {'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json'}
-  });
-
+  withCredentials: true,  
+  headers: {
+    'x-xsrf-token':  someCsrfToken,
+  }
+});
 export default newRequest;
