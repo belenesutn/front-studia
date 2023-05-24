b460d37dcffd3a4cd794978e85da6296c95e221b
Studia <mpartenzi@frd.utn.edu.ar>
2023-05-24 18:34:26 -0300

    Fix class

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const newRequest = axios.create({
   baseURL: "https://studia-utn.uc.r.appspot.com/api/",
   withCredentials: true,
-});
+  headers: {'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json'}
+  });
 
 export default newRequest;
