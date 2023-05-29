65fec224b8491400e19487c7e4fe52350aa9a029
Studia <mpartenzi@frd.utn.edu.ar>
2023-05-29 19:53:13 -0300

    Creo que funciona #Cursed

--- src/pages/edit/Edit.jsx
+++ src/pages/edit/Edit.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import React from "react";
+import newRequest from "../../utils/newRequest";
 import { useNavigate } from "react-router-dom";
 import getCurrentUser from "../../utils/getCurrentUser";
 import "./Edit.scss"
